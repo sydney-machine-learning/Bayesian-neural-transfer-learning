5340dd713b5091a32c4bd70c9017da05e0475578
arpit-kapoor <kapoor.arpit97@gmail.com>
2018-07-09 16:17:35 +1000

    weights distribution added

--- transfer-learning/mcmc-transfer-learning/w_hist.py
+++ transfer-learning/mcmc-transfer-learning/w_hist.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib as mplt
 import numpy as np
+import seaborn as sns
+from scipy.stats import norm
+
 mplt.style.use('bmh')
 
 mplt.rcParams.update({'font.size': 10})
@@ -9,24 +12,19 @@ mplt.rc('ytick', labelsize=13)
 
 weights = np.genfromtxt('weights.csv', delimiter=',')
 weights_trf = np.genfromtxt('weights_trf.csv', delimiter=',')
-print(weights)
-#
-# plt.hist(weights, bins=50, alpha=0.5, facecolor='sandybrown')
-# plt.xlabel('Parameter value')
-# plt.ylabel('Frequency')
-# plt.savefig('weight.png')
-# plt.clf()
-#
-# plt.hist(weights_trf, bins = 50, alpha=0.5, facecolor='sandybrown')
-# plt.xlabel('Parameter value')
-# plt.ylabel('Frequency')
-# plt.savefig('weight_trf.png')
-# plt.clf()
+
+# def calc_posterior_analytical(data, x, mu_0, sigma_0):
+#     sigma = 1.
+#     n = len(data)
+#     mu_post = (mu_0 / sigma_0**2 + data.sum() / sigma**2) / (1. / sigma_0**2 + n / sigma**2)
+#     sigma_post = (1. / sigma_0**2 + n / sigma**2)**-1
+#     return norm(mu_post, np.sqrt(sigma_post)).pdf(x)
 
 for index in range(weights.shape[1]):
     ax = plt.subplot(111)
-    plt.hist(weights_trf[:,index], bins=50, alpha=0.5, facecolor='sandybrown', label='no-transfer')
-    plt.hist(weights[:, index], bins=50, alpha=0.5, facecolor='C0', label='transfer')
+    x = np.linspace(-.5, .5, 500)
+    plt.hist(weights_trf[400:,index], bins=50, alpha=0.5, facecolor='sandybrown', label='no-transfer', density=True)
+    plt.hist(weights[400:, index], bins=50, alpha=0.5, facecolor='C0', label='transfer', density=True)
     plt.legend()
     plt.xlabel('Parameter value')
     plt.ylabel('Frequency')
